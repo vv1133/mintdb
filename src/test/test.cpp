6b47585b03fb5ea7f6e87502ac49fb8868c15173
vv1133 <zhengdi_vv1133@foxmail.com>
2014-01-09 22:38:55 +0800

    add ossMmapFile

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,13 +1,21 @@
 #include <string.h>
-#include "ossPrimitiveFileOp.hpp"
+//#include "ossPrimitiveFileOp.hpp"
+#include "ossMmapFile.hpp"
 
 int main ( int argc, char **argv )
 {
 	const char *str = "hello world\n";
-	ossPrimitiveFileOp *file = new ossPrimitiveFileOp();
-	file->Open("hello.txt", OSS_PRIMITIVE_FILE_OP_OPEN_ALWAYS);
-	file->Write(str, strlen(str));
-	file->Close();
+//	ossPrimitiveFileOp *file = new ossPrimitiveFileOp();
+//	file->Open("hello.txt", OSS_PRIMITIVE_FILE_OP_OPEN_ALWAYS);
+//	file->Write(str, strlen(str));
+//	file->Close();
+	
+	void *pos;
+	ossMmapFile *mapfile = new ossMmapFile();
+	mapfile->open("hello.txt", OSS_PRIMITIVE_FILE_OP_OPEN_ALWAYS);
+	mapfile->map(0, 4096, &pos);
+	strcpy((char*)pos, str);
+	mapfile->close();
 
 	return 0;
 }
